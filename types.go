a8d11fb642af4785676e37a4a5fd522a9af2dc6f
MartinSantiagoBarraza <martinsbarraza@gmail.com>
2020-04-01 03:51:11 -0300

    first commit

---
+++ types.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type MetaData interface {
+}
+
+type User struct {
+	Name  string
+	Email string
+	Phone string
+}
+
+func (u *User) ToJson() ([]byte, error) {
+	return json.Marshal(u)
+}
